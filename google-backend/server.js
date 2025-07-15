bdb5e8c4efbab50bbedf52bf2c14a128573712e1
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-07-15 17:25:45 +0530

    Revert "update"
    This reverts commit 32bd3390dff944d10a9c60ba17755759e46ae7c8.

--- google-backend/server.js
+++ google-backend/server.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
-import { configureGlobalMiddleware } from './config/global.js';
 
 dotenv.config();
 const app = express();
 
-// Apply global middleware configuration
-configureGlobalMiddleware(app);
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
